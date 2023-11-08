0411cac13bba44f6437834116e09f4ec9a88b4ef
Junseo11 <0302ser@naver.com>
2023-11-09 01:20:15 +0900

    std 11.9

--- b9095.java
+++ b9095.java
@@ -4,6 +4,8 @@ package baek.BaekjoonKotlin;
 //5 11111 1112 1121 1211 2111 113 131 311
 
 
+import java.util.Scanner;
+
 /*
 1   1
 2 11 2 2
@@ -12,5 +14,23 @@ package baek.BaekjoonKotlin;
 
  */
 public class b9095 {
+    public  static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int arr[] = new int[12];
+        arr[1] = 1;
+        arr[2] =2;
+        arr[3] =4;
+
+        for(int i = 4 ; i< arr.length; ++i){
+            arr[i] = arr[i-1]+arr[i-2]+arr[i-3];
+        }
+
+        for(int i =0; i<n; ++i){
+            int a = sc.nextInt();
+            System.out.println(arr[a]);
+        }
+    }
 
 }
